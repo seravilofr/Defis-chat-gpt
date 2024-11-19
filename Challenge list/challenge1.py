1650e6cc60ba8a1ba2a80cdf1ee40c4da2f529fc
François Olivares <156725976+seravilofr@users.noreply.github.com>
2024-11-19 16:37:19 +0000

    cleaned code

--- Challenge list/challenge1.py
+++ Challenge list/challenge1.py
@@ -1,20 +1,3 @@
-# **Premier défi : Calculatrice de somme simple**
-
-# Écris un programme Python qui :
-
-# - Demande deux nombres à l'utilisateur.
-# - Additionne ces deux nombres.
-# - Affiche le résultat.
-
-# Ce que tu apprends :
-
-# - Les entrées et sorties (input/output) : Comprendre comment demander    une donnée à l'utilisateur (input) et afficher un résultat (print).
-# - Les variables : Stocker et manipuler des valeurs.
-# - Les types de données : Convertir des entrées en nombres (int ou float).
-
-# C'est un exercice de base pour t'initier à la logique de programmation et à la syntaxe de Python. 
-
-
 number_1 = input("Enter a number : ")
 number_2 = input("Enter a second number : ")
 
--- Challenge list/challenge2.py
+++ Challenge list/challenge2.py
@@ -1,22 +1,3 @@
-# Défi 2 : Convertisseur de températures
-
-# Écris un programme Python qui :
-
-#     Demande à l'utilisateur une température en degrés Celsius.
-#     Convertit cette température en degrés Fahrenheit.
-#     Affiche le résultat.
-
-# La formule de conversion est :
-# F=C×(9/5)+32
-
-# Ce que tu apprends avec ce défi :
-
-#     Opérations mathématiques : Comment utiliser des formules dans Python.
-#     Sortie formatée : Afficher un résultat avec du texte pour rendre la sortie plus lisible.
-#     S'assurer que tu maîtrises les bases : Manipulation des entrées, des variables et des types de données.
-
-
-
 #On peut directement convertir l'input en float avec float(input()
 celsius = float(input("Enter the temperature in Celsius : "))
 
