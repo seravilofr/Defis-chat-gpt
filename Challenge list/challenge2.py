3f2ab4ea0bf8d5be370715da6e8271775cc58793
François Olivares <156725976+seravilofr@users.noreply.github.com>
2024-11-19 16:33:35 +0000

    challenge2 completed

--- Challenge list/challenge2.py
+++ Challenge list/challenge2.py
@@ -14,3 +14,15 @@
 #     Opérations mathématiques : Comment utiliser des formules dans Python.
 #     Sortie formatée : Afficher un résultat avec du texte pour rendre la sortie plus lisible.
 #     S'assurer que tu maîtrises les bases : Manipulation des entrées, des variables et des types de données.
+
+
+
+#On peut directement convertir l'input en float avec float(input()
+celsius = float(input("Enter the temperature in Celsius : "))
+
+#Du coup pas besoin de convertir celsius en float puisqu'on l'a déjà fait sur l'étape du dessus
+fahrenheit = celsius*(9/5)+32
+
+#Plutôt que print("Temperature in Fahrenheit:", fahrenheit,"°F") on utilise des f-strings qui permettent d'insérer directement des variables dans une chaîne :
+print(f"Temperature in Celsius: {celsius}°C")
+print(f"Temperature in Fahrenheit: {fahrenheit}°F")
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-celsius = input("Enter the temperature in Celsius : ")
-fahrenheit = float(celsius)*(9/5)+32
-print ("Temperature in Celsius:", float(celsius),"°C")
-print("Temperature in Fahrenheit:", fahrenheit,"°F")
+#On peut directement convertir l'input en float avec float(input()
+celsius = float(input("Enter the temperature in Celsius : "))
 
+#Du coup pas besoin de convertir celsius en float puisqu'on l'a déjà fait sur l'étape du dessus
+fahrenheit = celsius*(9/5)+32
+
+#Plutôt que print("Temperature in Fahrenheit:", fahrenheit,"°F") on utilise des f-strings qui permettent d'insérer directement des variables dans une chaîne :
+print(f"Temperature in Celsius: {celsius}°C")
+print(f"Temperature in Fahrenheit: {fahrenheit}°F")
