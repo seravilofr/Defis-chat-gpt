09091fdeb834361a62fbd46367334b1fdc986415
François Olivares <156725976+seravilofr@users.noreply.github.com>
2024-11-20 15:03:17 +0000

    challenge 4 translation

--- Challenge list/challenge4.py
+++ Challenge list/challenge4.py
@@ -1,27 +1,27 @@
 try:
-  nb_notes = int(input("Combien de notes à saisir? "))
-  print(f"Vous allez saisir {nb_notes} notes")
+  grades_nb = int(input("How many grades to input? "))
+  print(f"You will input {grades_nb} grades")
 
-  notes_saisies = [] #crée liste "notes"
+  grades_input = [] #crée liste "notes"
 
-  for i in range(nb_notes):
+  for i in range(grades_nb):
       while True: #redemande en boucle une note qui ne serait pas bonne
         try:
-          note = float(input(f"Note n°{i+1} : "))
-          notes_saisies.append(note)
+          grade = float(input(f"Grade n°{i+1} : "))
+          grades_input.append(grade)
           break  # quitte la boucle si la saisie est valide
         except ValueError:
-          print("Vous n'avez pas saisi une note valide")
+          print("You did not input a correct grade")
 
 
 
-  print(f"Voici les notes saisies : {notes_saisies}") #mettre ce print hors de la boucle for pour que la liste s'affiche une seule fois
+  print(f"Here are the grades : {grades_input}") #mettre ce print hors de la boucle for pour que la liste s'affiche une seule fois
 
-  somme = sum(notes_saisies)
-  moyenne = round(somme/nb_notes, 2)
-  print(f"Voici la moyenne des {nb_notes} notes: {moyenne}")
+  somme = sum(grades_input)
+  moyenne = round(somme/grades_nb, 2)
+  print(f"Here is the average of the {grades_nb} grades: {moyenne}")
 
 
 #Le bloc except fonctionne pour tout le code
 except ValueError:
-  print("Vous n'avez pas saisi un nombre valide")
+  print("You did not input a correct number of grades")
